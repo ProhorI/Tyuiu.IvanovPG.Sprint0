bb1ffad2dee3f1087667ee64ade7a749deb24fb2
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-09-15 16:53:31 +0500

    Task5

---
+++ Tyuiu.IvanovPG.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿namespace Tyuiu.IvanovPG.Sprint0.Task5.V0.Lib;
+
+public class DataService
+{
+    public static int Addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Division(int a, int b)
+    {
+        if (b == 0)
+        {
+            //пример создания в целях демонстрации ветвления
+            //в реальных проектах нужно использовать Exception
+            Console.WriteLine("Переменная b == {0} на ноль делить нельзя", b);
+            return -1;
+        }
+        else 
+        {
+            return a / b;
+        } 
+    }
+}
