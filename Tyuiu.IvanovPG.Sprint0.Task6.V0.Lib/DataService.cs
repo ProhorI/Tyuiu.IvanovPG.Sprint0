8fe0ac025d58404f5f7365d50abbae1f07e81aa8
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-09-15 19:14:44 +0500

    Task6

---
+++ Tyuiu.IvanovPG.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,47 @@
+﻿namespace Tyuiu.IvanovPG.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        // пример циклической структуры (цикл с парметром)  for
+        public static object AdditionArray(int[] number)
+        {
+            var total = 0;
+            for (var i = 0; i < number.Length; i++)
+            {
+                total = total + number[i];
+            }
+            return total;
+
+        }
+        // пример циклической структуры (цикл с предусловием) while 
+        public static object SubstractionArray(int[] number)
+        {
+            var total = 0;
+            var index = 0;
+
+
+            while (index < number.Length)
+            {
+                total = total - number[index];
+                index++;
+            }
+            return (total);
+        }
+
+        // пример циклической структуры (цикл с постусловием) do while 
+        public static object MultiplicationArray(int[] number)
+        {
+            var total = 1;
+            var index = 0;
+            do
+            {
+                total = total * number[index];
+                index++;
+            }
+            while (index < number.Length);
+
+
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.IvanovPG.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.IvanovPG.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.IvanovPG.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            // пример циклической структуры находится в библиотеке классов в методе AdditionArray
+            Console.WriteLine("Сумма Элементов массива = " + DataService.AdditionArray(numsArray));
+
+            // пример циклической структуры находится в библиотеке классов в методе SubstractionArray
+            Console.WriteLine("Разность Элементов массива = " + DataService.SubstractionArray(numsArray));
+
+            // пример циклической структуры находится в библиотеке классов в методе MultiplicationArray
+            Console.WriteLine("Произведение Элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
